798aabe6c2e38bf8578c87eccd1dd1faef46909d
kooler62 <kooler62@gmail.com>
2017-12-01 15:44:41 +0200

    REFACT

--- lib/exblockchaininfo.ex
+++ lib/exblockchaininfo.ex
@@ -2,53 +2,39 @@ defmodule Exblockchaininfo do
   @moduledoc """
   Documentation for Exblockchaininfo.
   """
-import Http
+import Httpblockchaininfo
 
-  @doc """
-  Total wallet balance
-  """
-  def balance do
-    Http.wallet_balance
+  def wallet_balance do
+    Httpblockchaininfo.request("balance", parameters_get)
   end
 
-  @doc """
-  create new adress with label, do this parametr not required
-  """
-  def adress_new(label \\ "") do
-    Http.address_create(label)
-  end
-
-  @doc """
-  listing your addresses
-  """
-  def list do
-    Http.list
+  def address_create(label \\ "") do
+    params = parameters_get("adress_create", label)
+    Httpblockchaininfo.request("new_address", params)
   end
 
   def address_balance(address) do
-    Http.address_balance(address)
+    params = parameters_get("address_balance", address)
+    Httpblockchaininfo.request("address_balance", params)
   end
 
   def address_archive(address) do
-    Http.address_archive(address)
+    params = parameters_get("address_archive", address)
+    Httpblockchaininfo.request("archive_address", params)
   end
 
   def address_unarchive(address) do
-    Http.address_unarchive(address)
+    params = parameters_get("address_balance", address)
+    Httpblockchaininfo.request("unarchive_address", params)
   end
 
-  # This method dont testing after refactoring because dont want spell BTC
-  def sent_btc_to_one(amount, to, fee \\ 10000) do
-    #send_btc(amount, to, fee \\ 10000)
+  def list do
+    Httpblockchaininfo.request("list", parameters_get)
   end
 
-  @doc """
-  Adress  balance
-  """
-  def balance(address) do
-    #  This endpoint has been deprecated, for the best safety and security,
-    #  use the HD API instead:
-    #  https://github.com/blockchain/service-my-wallet-v3#enable-hd-functionality\
+  def payment(amount, fee \\ "10000", to) do
+    params = parameters_get("payment", amount, fee, to)
+    Httpblockchaininfo.request("payment", params)
   end
 
 end
