006a166258207229e312f21a2a07e531941ebf4e
kooler62 <kooler62@gmail.com>
2017-12-06 16:10:32 +0200

    triggers tests

--- test/ex_blockchain_info/wallet_test.exs
+++ test/ex_blockchain_info/wallet_test.exs
@@ -32,6 +32,104 @@ defmodule ExBlockchainInfo.WalletTest do
     "http://localhost:3000/merchant/guid/balance"
   end
 
+  test "create_address('label') triggers correct url" do
+    url = create_address_url()
+    params = %{"password" => nil,"second_password" => nil, "label"=>"label" } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.create_address("label")
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  test "create_address() triggers correct url" do
+    url = create_address_url()
+    params = %{"password" => nil,"second_password" => nil, "label"=>"" } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.create_address()
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  defp create_address_url do
+    "http://localhost:3000/merchant/guid/new_address"
+  end
+
+  test "get_address_balance('address') triggers correct url" do
+    url = get_address_balance_url()
+    params = %{"password" => nil,"second_password" => nil, "address"=>"1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq" } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.get_address_balance("1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq")
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  defp get_address_balance_url do
+    "http://localhost:3000/merchant/guid/address_balance"
+  end
+
+  test "archive_address('adress') triggers correct url" do
+    url = archive_address_url()
+    params = %{"password" => nil,"second_password" => nil, "address"=>"1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq" } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.archive_address("1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq")
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  defp archive_address_url do
+    "http://localhost:3000/merchant/guid/archive_address"
+  end
+
+  test "unarchive_address('adress') triggers correct url" do
+    url = unarchive_address_url()
+    params = %{"password" => nil,"second_password" => nil, "address"=>"1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq" } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.unarchive_address("1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq")
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  defp unarchive_address_url do
+    "http://localhost:3000/merchant/guid/unarchive_address"
+  end
+
+  test "get_addresses_list() triggers correct url" do
+    url = get_addresses_list_url()
+    params = %{"password" => nil } |> Poison.encode!
+    headers = %{"Content-Type" => "application/json"}
+    mock = [post: fn(url, params, headers) -> successful_response() end]
+
+    with_mock HTTPoison, mock do
+      ExBlockchainInfo.Wallet.get_addresses_list()
+
+      assert called HTTPoison.post(url, params, headers)
+    end
+  end
+
+  defp get_addresses_list_url do
+    "http://localhost:3000/merchant/guid/list"
+  end
+
   defp successful_response do
     {:ok,
       %HTTPoison.Response{
